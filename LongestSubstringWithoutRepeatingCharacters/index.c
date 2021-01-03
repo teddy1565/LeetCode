c26a5b4e8fb665d09610dad90263a90ba52f04e1
teddy1565 <teddy1565@gmail.com>
2021-01-03 23:21:29 +0800

    20210103

--- LongestSubstringWithoutRepeatingCharacters/index.c
+++ LongestSubstringWithoutRepeatingCharacters/index.c
@@ -4,7 +4,14 @@
 //Given a string s, find the length of the longest substring without repeating characters.
 int lengthOfLongestSubstring(char *s){
     
+    return 1;
 }
 int main(){
+    int strleng=0;
+    scanf("%d",&strleng);
+    char *input;
+    input = malloc(sizeof(char)*(strleng+1));
+    fgets(input,strleng,stdin);
+    printf("%d",lengthOfLongestSubstring(input));
     return 0;
 }
