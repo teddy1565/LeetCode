e4f1aef899626e55a36e3a8265568157423ee491
teddy1565 <teddy1565@gmail.com>
2021-01-04 15:33:49 +0800

    4 ms/5.8MB

--- LongestSubstringWithoutRepeatingCharacters/index.c
+++ LongestSubstringWithoutRepeatingCharacters/index.c
@@ -3,15 +3,32 @@
 #include<string.h>
 //Given a string s, find the length of the longest substring without repeating characters.
 int lengthOfLongestSubstring(char *s){
-    
-    return 1;
+    char *p = &s[0];
+    int counter=0;
+    if(s[0]=='\0')return 0;
+    for(int i=0;s[i]!='\0';i++){
+        if(s[i+1]=='\0'&&i!=0)return 1;
+        if(s[i]!=s[i+1])break;
+    }
+    for(int i=0,offset=0;s[i]!='\0';i++){
+        int subCounter=0;
+        for(int j=0+offset;p[j]!='\0'&&j<i;j++){
+            if(p[j]==s[i]){
+                offset=j+1;
+                break;
+            }
+            subCounter++;
+        }
+        if(subCounter>counter)counter = subCounter;
+    }
+    return counter+1;
 }
 int main(){
-    int strleng=0;
-    scanf("%d",&strleng);
-    char *input;
-    input = malloc(sizeof(char)*(strleng+1));
-    fgets(input,strleng,stdin);
+    char input[100];
+    fgets(input,100,stdin);
+    for(int i=0;i<100;i++){
+        if(input[i]=='\n')input[i]='\0';
+    }
     printf("%d",lengthOfLongestSubstring(input));
     return 0;
 }
