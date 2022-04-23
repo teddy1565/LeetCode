1db75786c9718bbda5d7c604e417cb1d06ae14ef
teddy1565 <teddy1565@gmail.com>
2022-04-23 12:22:09 +0800

    暴力法虛擬碼

--- Longest Palindromic Substring/index_v4.c
+++ Longest Palindromic Substring/index_v4.c
@@ -4,9 +4,33 @@
 #include <stdio.h>
 
 char *longestPalindrome(char *s) {
+    int length = 0;
+    
+    for(int i=0;s[i]!='\0';i++) {
+        length++;
+    }
+    char **tmp = (char**) malloc(sizeof(char) * length);
 
+    for (int i=0;i<length;i++) {
+        int len = 1;
+        int start = 0;
+        int end = 0;
+        for (int offset=1;s[i-offset] == s[i+offset]; offset++) {
+            len+=2;
+            start = i-offset;
+            end = i+offset;
+        }
+        tmp[i] = (char*) malloc(sizeof(char) * (len+1));
+        tmp[i][len-1] = '\0';
+        for (int j=start,k=0;j<=end;j++,k++) {
+            tmp[i][k] = s[j];
+        }
+        printf("%s\n",tmp[i]);
+    }
+    return s;
 }
 
 int main(void) {
+    longestPalindrome("hello world");
     return 0;
 }
