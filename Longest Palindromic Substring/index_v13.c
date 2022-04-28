02b64c2190f0bf807cc30636accf3cd9505a1b80
teddy1565 <teddy1565@gmail.com>
2022-04-29 05:48:24 +0800

    0429

---
+++ Longest Palindromic Substring/index_v13.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *lognestPalindrome(char *s) {
+    int length = strlen(s);
+    return s;
+}
+
+int main(void) {
+    lognestPalindrome("hello world");
+    return 0;
+}   
