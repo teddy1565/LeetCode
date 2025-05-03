a5af40a0c69fd83ca134ef7414733414097d1223
teddy1565 <teddy1565@gmail.com>
2025-05-03 12:03:00 +0800

    refactor: 💡 clean

--- Zigzag Conversion/index.c
+++ Zigzag Conversion/index.c
@@ -16,18 +16,18 @@ char* convert(char* s, int numRows) {
         return s;
     }
 
-    const int source_string_length = strlen(s) + 1;
+    const int source_string_length = strlen(s);
     
     if (numRows == 2) {
-        char *temp = (char*) malloc(sizeof(char) * source_string_length);
+        char *temp = (char*) malloc(sizeof(char) * (source_string_length + 1));
         int j = 0;
-        for (int i=0; i < (source_string_length-1); i+=2, j++) {
+        for (int i=0; i < (source_string_length); i+=2, j++) {
             temp[j] = s[i];
         }
-        for (int i=1; i < source_string_length; i+=2, j++) {
+        for (int i=1; i < (source_string_length + 1); i+=2, j++) {
             temp[j] = s[i];
         }
-        temp[source_string_length-1] = '\0';
+        temp[source_string_length] = '\0';
         for (int i=0; i < (strlen(temp) + 1); i++) {
             s[i] = temp[i];
         }
@@ -36,30 +36,26 @@ char* convert(char* s, int numRows) {
     }
 
     const int space_size = numRows - 2;
-    const int amplitude[2] = {numRows, space_size};
-    char *answer = (char*) malloc(sizeof(char) * (source_string_length));
-    
+    char *answer = (char*) malloc(sizeof(char) * (source_string_length + 1));
     const int zig_interval_size = space_size + numRows;
+    
     int k = 0;
     for (int i=0; i < numRows; i++) {
-        for (int zig_header_index = i; zig_header_index < source_string_length; zig_header_index += zig_interval_size) {
+        for (int zig_header_index = i; zig_header_index < (source_string_length + 1); zig_header_index += zig_interval_size) {
             int next_zig_top_header_index = (zig_header_index + zig_interval_size) - i;
             int offset = zig_header_index % zig_interval_size;
-            if (zig_header_index >= (source_string_length-1)) {
+            if (zig_header_index >= (source_string_length + 1)) {
                 break;
             }
             if (i != 0) {
-                printf("%c", s[zig_header_index]);
                 answer[k] = s[zig_header_index];
                 k++;
                 
                 if (next_zig_top_header_index - offset == zig_header_index) {
                     continue;
-                } else if (next_zig_top_header_index > source_string_length) {
+                } else if (next_zig_top_header_index > (source_string_length + 1)) {
                     break;
                 }
-
-                printf("%c", s[zig_header_index]);
                 answer[k] = s[next_zig_top_header_index - offset];
                 k++;
             } else {
@@ -69,11 +65,11 @@ char* convert(char* s, int numRows) {
         }
     }
 
-    for (int i = 0; i < source_string_length-1; i++) {
+    for (int i = 0; i < source_string_length; i++) {
         s[i] = answer[i];
     }
 
-    s[source_string_length-1] = '\0';
+    s[source_string_length] = '\0';
 
     free(answer);
 
