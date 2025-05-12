675dd8ea2266cc097cda6d1ccecb33dc44481282
Teddy Xiong <teddy1565@gmail.com>
2025-05-12 13:51:11 +0800

    fix: 🐛 it parse pattern string not source string

--- Regular Expression Matching/index.c
+++ Regular Expression Matching/index.c
@@ -25,7 +25,7 @@ bool isMatch(char* s, char* p) {
     // if no any pattern
     bool find_pattern = false;
     for (int i = 0; i < source_string_length; i++) {
-        if (s[i] == split_mark_star || s[i] == split_mark_dot) {
+        if (p[i] == split_mark_star || p[i] == split_mark_dot) {
             find_pattern = true;
             break;
         }
@@ -57,35 +57,36 @@ bool isMatch(char* s, char* p) {
 
     // 使用prev紀錄上一個是屬於string或pattern的狀態, 0表示string, 1表示pattern
     for (int i = 0, prev = 0, temp_s_k = 0, temp_p_k = 0; i < source_string_length + 1; i++) {
-        if (s[i] == '\0') {
+        if (p[i] == '\0') {
             temp_s[temp_s_k] = '\0';
             temp_p[temp_p_k] = '\0';
             break;
         }
-        if ((s[i] == split_mark_dot && prev == 0) || (s[i] == split_mark_star && prev == 0)) {
+        if ((p[i] == split_mark_dot && prev == 0) || (p[i] == split_mark_star && prev == 0)) {
             temp_s[temp_s_k] = '\0';
             temp_s_k++;
             s_group_length++;
             continue;
         }
-        if ((s[i] != split_mark_dot && prev == 1) && (s[i] != split_mark_star && prev == 1)) {
+        if ((p[i] != split_mark_dot && prev == 1) && (p[i] != split_mark_star && prev == 1)) {
             temp_p[temp_p_k] = '\0';
             temp_p_k++;
             p_group_length++;
             continue;
         }
-        if (s[i] != split_mark_dot && s[i] != split_mark_star) {
-            temp_s[temp_s_k] = s[i];
+        if (p[i] != split_mark_dot && p[i] != split_mark_star) {
+            temp_s[temp_s_k] = p[i];
             temp_s_k++;
             continue;
         }
-        if (s[i] == split_mark_dot || s[i] == split_mark_star) {
-            temp_p[temp_p_k] = s[i];
+        if (p[i] == split_mark_dot || p[i] == split_mark_star) {
+            temp_p[temp_p_k] = p[i];
             temp_p_k++;
             continue;
         }
     }
 
+    // 將分類好的組合對應與解析
     for (int i=0, j=0; (i < s_group_length) && (j < p_group_length); ) {
 
     }
