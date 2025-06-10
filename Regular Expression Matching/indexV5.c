eb9d355f5ff41e2d529e3cd67dfc45aa4f8fd9c0
Teddy Xiong <teddy1565@gmail.com>
2025-06-10 18:46:32 +0800

    fix: 🐛 add more eq

--- Regular Expression Matching/indexV5.c
+++ Regular Expression Matching/indexV5.c
@@ -15,7 +15,7 @@ bool isMatch(char* s, char* p) {
 
     // if no any pattern
     bool find_pattern = false;
-    for (int i = 0; i < source_str_length; i++) {
+    for (int i = 0; i < pattern_str_length; i++) {
         if (p[i] == split_mark_star || p[i] == split_mark_dot) {
             find_pattern = true;
             break;
@@ -47,7 +47,7 @@ bool isMatch(char* s, char* p) {
 
     // ==== 結束pattern如果沒有split_mark檢查
 
-
+    printf("GG\n");
     char *must_equal = (char *)malloc(sizeof(char) * 21);
     char *str_header = must_equal;
     memset(str_header, 0, 21);
@@ -56,19 +56,23 @@ bool isMatch(char* s, char* p) {
     int match_index = 0;
     for (int i = 1; i <= pattern_str_length; i++) {
         if (p[i] == '*') {
-            
             // 先處理必須一樣的部分
             pattern_chr = p[i - 1];
             *(must_equal)++ = '\0';
-            for (int k = 0; k < strlen(str_header); k++) {
-                if (s[match_index + k] != str_header[k] && str_header[k] != '.') {
+            for (int k = 0, g = 0; k < strlen(str_header); k++) {
+                if (str_header[k] == '.') {
+                    continue;
+                } else if (str_header[k] != '.' && s[match_index + g] != str_header[k]) {
+                    printf("%c\t%c\t%d\t%d\n", str_header[k], s[match_index + g] ,match_index, g);
+                    free(str_header);
                     return false;
                 }
+                match_index++;
+                g++;
             }
-            match_index = match_index + (strlen(str_header));
+            // match_index = match_index + (strlen(str_header));
             must_equal = str_header;
             // 接著處理在意的部分
-            printf("%d\n", match_index);
             if (pattern_chr == '.' && (i + 1) < pattern_str_length) {
                 char n = p[i + 1];
                 while(match_index < source_str_length && s[match_index] != n) {
@@ -86,6 +90,7 @@ bool isMatch(char* s, char* p) {
             i++;
         } else if (p[i] == '\0') {
             if (match_index == source_str_length && match_index == pattern_str_length) {
+                free(str_header);
                 return true;
             }
             
@@ -99,8 +104,8 @@ bool isMatch(char* s, char* p) {
             match_index = match_index + (strlen(str_header));
             
             must_equal = str_header;
-            printf("%d\n", match_index);
             if (match_index != source_str_length) {
+                free(str_header);
                 return false;
             }
         } else {
@@ -113,16 +118,19 @@ bool isMatch(char* s, char* p) {
 
 
 int main(void) {
-    printf("res: %d\n", isMatch("ab", ".*"));   //1
+    printf("[res: %d\n", isMatch("ab", ".*"));   //1
 
-    printf("res: %d\n", isMatch("aab", "c*a*b"));   //1
+    printf("[res: %d\n", isMatch("aab", "c*a*b"));   //1
     
-    printf("res: %d\n", isMatch("acbdb", "aa*cbdb"));   //1
-    printf("res: %d\n", isMatch("mississippi", "mis*is*p*.")); // 0
-    printf("res: %d\n", isMatch("mississippi", "mis*is*p*.")); // 0
-    printf("res: %d\n", isMatch("abcd", "d*"));// 0
-    printf("res: %d\n", isMatch("aaa", "a*a")); // 1
-    printf("res: %d\n", isMatch("ab", ".*c")); // 0
-    printf("res: %d\n", isMatch("aaa", "ab*a*c*a")); // 1
+    printf("[res: %d\n", isMatch("acbdb", "aa*cbdb"));   //1
+    printf("[res: %d\n", isMatch("mississippi", "mis*is*p*.")); // 0
+    printf("[res: %d\n", isMatch("mississippi", "mis*is*p*.")); // 0
+    printf("[res: %d\n", isMatch("abcd", "d*"));// 0
+    printf("[res: %d\n", isMatch("aaa", "a*a")); // 1
+    printf("[res: %d\n", isMatch("ab", ".*c")); // 0
+    printf("[res: %d\n", isMatch("aaa", "ab*a*c*a")); // 1
+    printf("[res: %d\n", isMatch("a", "ab*")); // 1
+    printf("[res: %d\n", isMatch("a", ".*..a*")); // 1
+    printf("[res: %d\n", isMatch("mississippi", "mis*is*ip*.")); // 1
     return 0;
 }
