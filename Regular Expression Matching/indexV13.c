af9da3542feec64d19f0f71afa0496609925a13d
Teddy Xiong <teddy1565@gmail.com>
2025-06-16 17:28:42 +0800

    feat: 🎸 show vector state info

--- Regular Expression Matching/indexV13.c
+++ Regular Expression Matching/indexV13.c
@@ -13,8 +13,14 @@ bool isMatch(char *s, char *p) {
 
     for (int i=0; i < pattern_str_length; i++) {
         for (int j=0; j < source_str_length; j++) {
-            if (p[i] == s[j]) {
+            if (p[i] == s[j] || p[i] == '.') {
                 v[i][j] = 1;
+            } else if (p[i] == '*') {
+                if (s[j] == p[i - 1] || p[i - 1] == '.') {
+                    v[i][j] = 1;
+                } else {
+                    v[i][j] = 0;
+                }
             } else if (p[i] != s[j]) {
                 v[i][j] = 0;
             }
@@ -31,6 +37,10 @@ bool isMatch(char *s, char *p) {
 
     for (int i=0; i < 20; i++) {
         for (int j=0; j < 20; j++) {
+            if (v[i][j] == 0) {
+                printf("  ");
+                continue;
+            }
             printf("%d ", v[i][j]);
         }
         if (i < pattern_str_length) {
@@ -45,8 +55,10 @@ bool isMatch(char *s, char *p) {
 }
 
 int main(void) {
-    isMatch("aa", "a*");
-    // isMatch("mississippi", "mis*is*p*.");
-    // isMatch("mississippi", "mis*is*ip*.");
+    isMatch("aa", "a*"); // true
+    isMatch("mississippi", "mis*is*ip*."); // true
+    isMatch("mississippi", "mis*is*p*."); // false
+    isMatch("bbcacbabbcbaaccabc", "b*a*a*.c*bb*b*.*.*"); // true
+    isMatch("bbacbcabbbbbcacabb", "aa*c*b*a*.*a*a.*."); // false
     return 0;
 }
