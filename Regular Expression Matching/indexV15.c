2c98b750d6d666f169e248f8d8439b9553c34f16
Teddy Xiong <teddy1565@gmail.com>
2025-06-19 16:41:32 +0800

    feat: 🎸 init

---
+++ Regular Expression Matching/indexV15.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool isMatch(char *s, char *p) {
+    
+}
+
+int main(void) {
+    // isMatch("abd", "ab*a"); // false
+    isMatch("bbcbbcbcbbcaabcacb", "a*.*ac*a*a*.a..*.*"); // false
+    // isMatch("asdvcavsdrfte", ".");
+    return 0;
+}
