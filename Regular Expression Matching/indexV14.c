c2db08b46854db4fd389cb24eeb09057d9a237cd
Teddy Xiong <teddy1565@gmail.com>
2025-06-19 16:03:34 +0800

    feat: 🎸 V14 test

---
+++ Regular Expression Matching/indexV14.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+bool isMatch(char *s, char *p) {
+    bool result = false;
+    printf("==========================================\n\n");
+    const int source_str_length = strlen(s);
+    const int pattern_str_length = strlen(p);
+
+    printf("s: %d\n", strlen(s));
+    printf("p: %d\n", strlen(p));
+
+    int v[20][20] = { 0 };
+
+    for (int i=0; i < pattern_str_length; i++) {
+        for (int j=0; j < source_str_length; j++) {
+            if (p[i] == s[j] || p[i] == '.') {
+                v[i][j] = 1;
+            } else if (p[i] == '*') {
+                if (s[j] == p[i - 1] || p[i - 1] == '.') {
+                    v[i][j] = 1;
+                } else {
+                    v[i][j] = 0;
+                }
+            } else if (p[i] != s[j]) {
+                v[i][j] = 0;
+            }
+        }
+    }
+
+    for (int i=0; i < pattern_str_length; i++) {
+        if (p[i] == '*') {
+            continue;
+        }
+        if (v[i][0] == 0 && p[i + 1] != '*') {
+            result = false;
+            break;  // 代表根本不通
+        }
+        if (v[i][0] == 1) {
+            for (int j=0, k=0; j < source_str_length; j++) {
+                if (v[i][j] == 0) {
+                    break;
+                }
+
+                if (((i + 1) < pattern_str_length) && ((j + 1) < source_str_length)) {
+                    if (p[i + 1] != '*' && v[i + 1][j + 1] == 1) {
+                        printf("[%d %d]: %d\n", i + 1, j + 1, v[i + 1][j + 1]);
+                    } else if (p[i + 1] == '*') {   // 如果匹配到 '*'
+                        if ((i + 2) < pattern_str_length && v[i + 2][j + 1] == 1) { // 而且剩餘pattern長度還足夠
+                            printf("[%d %d]: %d\n", i + 2, j + 1, v[i + 2][j + 1]);
+                        } else if ((i + 2) >= pattern_str_length) {
+                            printf("[%d %d]: %d\n", i + 1, j + 1, v[i + 1][j + 1]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (int i=0; i < 20; i++) {
+        if (i < source_str_length) {
+            printf("%c ", s[i]);
+        }
+    }
+
+    printf("\n--------------------\n");
+
+    for (int i=0; i < 20; i++) {
+        // if (p[i] == '*') {
+        //     continue;
+        // }
+        for (int j=0; j < 20; j++) {
+            if (v[i][j] == 0) {
+                printf("  ");
+                continue;
+            }
+            printf("%d ", v[i][j]);
+        }
+        if (i < pattern_str_length) {
+            printf("| %c", p[i]);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n");
+
+    printf("answer: %d\n", result);
+
+    
+    return result;
+}
+
+int main(void) {
+    isMatch("a", "ab*a"); // false
+    // isMatch("bbcbbcbcbbcaabcacb", "a*.*ac*a*a*.a..*.*"); // false
+    // isMatch("asdvcavsdrfte", ".");
+    return 0;
+}
