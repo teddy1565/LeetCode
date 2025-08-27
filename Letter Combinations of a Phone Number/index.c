a53c0fbbba32f6b72dfddbbdc7c00bc52a9c8897
Teddy Xiong <teddy1565@gmail.com>
2025-08-27 18:15:47 +0800

    feat: 🎸 init

---
+++ Letter Combinations of a Phone Number/index.c
@@ -0,0 +1,22 @@
+#include "index.h"
+
+char** letterCombinations(char* digits, int* returnSize) {
+    
+    char **answer;
+    int digits_size = strlen(digits);
+    if (digits_size == 0) {
+        *returnSize = 0;
+        return answer;
+    }
+
+    for (int i = 0; i < digits_size; i++) {
+        
+    }
+    
+    return answer;
+}
+
+int main(void) {
+    printf("%d\n", '2');
+    return 0;
+}
---
+++ Letter Combinations of a Phone Number/index.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** letterCombinations(char* digits, int* returnSize);
