18d9506c78294c8b84ed6d62c35ee06241ba74e2
Teddy Xiong <teddy1565@gmail.com>
2025-08-28 19:46:22 +0800

    feat: 🎸 different trys

---
+++ Letter Combinations of a Phone Number/index_v5.c
@@ -0,0 +1,133 @@
+/**
+ * @file index_v5.c
+ * @author JING TING XIONG (teddy1565@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-08-28
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ * 原本打算用暴力法解決
+ * 
+ * 後來有點意識到，似乎可以從最後面做起
+ * 
+ */
+#include "index.h"
+
+#define DEBUG
+
+
+#define CHR_OFFSET 50
+
+const int digits_length_map[8] = { 3, 3, 3, 3, 3, 4, 3, 4 };
+
+const char key_board[8][5] = {
+    { 'a', 'b', 'c', '\0', '\0' },
+    { 'd', 'e', 'f', '\0', '\0' },
+    { 'g', 'h', 'i', '\0', '\0' },
+    { 'j', 'k', 'l', '\0', '\0' },
+    { 'm', 'n', 'o', '\0', '\0' },
+    { 'p', 'q', 'r', 's' , '\0'},
+    { 't', 'u', 'v', '\0', '\0'},
+    { 'w', 'x', 'y', 'z' , '\0'}
+};
+
+
+char** letterCombinations(char* digits, int* returnSize) {
+    
+    char **answer;
+    int digits_size = strlen(digits);
+    if (digits_size == 0) {
+        *returnSize = 0;
+        return answer;
+    }
+
+    int total = 1;
+    int rows = 1;
+
+    for (int i = 0; i < digits_size; i++) {
+        if (i > 0) {
+            rows = rows * digits_length_map[digits[i] - CHR_OFFSET];
+        }
+        total = total * digits_length_map[digits[i] - CHR_OFFSET];
+    }
+
+    *returnSize = total;
+
+    int cols = total / rows;
+
+    #ifdef DEBUG
+    printf("%d %d %d\n", total, rows, cols);
+    #endif
+
+    answer = (char **) malloc(sizeof(char *) * total);
+    for (int i = 0; i < total; i++) {
+        answer[i] = (char *) malloc(sizeof(char) * (digits_size + 1));
+        memset(answer[i], 0, (sizeof(char) * (digits_size + 1)));
+    }
+
+    for (int i = 0; i < 1; i++) {
+        for (int j = 0; j < total; j++) {
+            int key_board_row = digits[i] - CHR_OFFSET;
+            answer[j][i] = key_board[key_board_row][j / rows];
+        }
+    }
+
+    int divisor = rows / cols; 
+    for (int i = 1; i < digits_size; i++) {
+        for (int j = 0; j < total; j++) {
+            int key_board_row = digits[i] - CHR_OFFSET;
+            // printf("%d %d %d\n", j, i, key_board_row);
+            answer[j][i] = key_board[key_board_row][(j / divisor) % digits_length_map[key_board_row]];
+        }
+        if (i < digits_size - 1) {
+            divisor = divisor / digits_length_map[digits[i] - CHR_OFFSET];
+        }
+    }
+
+
+
+    for (int i = 0; i < total; i++) {
+        for (int j = 0; j < digits_size; j++) {
+            printf("%c", answer[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("----------------\n");
+
+    
+    return answer;
+}
+
+int main(void) {
+    int return_size = 0;
+    // printf("2\n");
+    // printf("\t%d %d %d\n", 'a', 'b', 'c');
+
+    // printf("3\n");
+    // printf("\t%d %d %d\n", 'd', 'e', 'f');
+
+    // printf("4\n");
+    // printf("\t%d %d %d\n", 'g', 'h', 'i');
+
+    // printf("5\n");
+    // printf("\t%d %d %d\n", 'j', 'k', 'l');
+
+    // printf("6\n");
+    // printf("\t%d %d %d\n", 'm', 'n', 'o');
+
+    // printf("7\n");
+    // printf("\t%d %d %d %d\n", 'p', 'q', 'r', 's');
+
+    // printf("8\n");
+    // printf("\t%d %d %d\n", 't', 'u', 'v');
+
+    // printf("9\n");
+    // printf("\t%d %d %d %d\n", 'w', 'x', 'y', 'z');
+
+
+    // letterCombinations("7239", &return_size);
+    letterCombinations("234", &return_size);
+    return 0;
+}
---
+++ Letter Combinations of a Phone Number/index_v8.c
@@ -0,0 +1,142 @@
+/**
+ * @file index_v8.c
+ * @author JING TING XIONG (teddy1565@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-08-28
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include "index.h"
+
+#define DEBUG
+
+
+#define CHR_OFFSET 50
+
+const int digits_length_map[8] = { 3, 3, 3, 3, 3, 4, 3, 4 };
+const char key_board_head_map[8] = { 'a', 'd', 'g', 'j', 'm', 'p', 't', 'w' };
+
+const char key_board[8][5] = {
+    { 'a', 'b', 'c', '\0', '\0' },
+    { 'd', 'e', 'f', '\0', '\0' },
+    { 'g', 'h', 'i', '\0', '\0' },
+    { 'j', 'k', 'l', '\0', '\0' },
+    { 'm', 'n', 'o', '\0', '\0' },
+    { 'p', 'q', 'r', 's' , '\0'},
+    { 't', 'u', 'v', '\0', '\0'},
+    { 'w', 'x', 'y', 'z' , '\0'}
+};
+
+
+char** letterCombinations(char* digits, int* returnSize) {
+    
+    char **answer;
+    int digits_size = strlen(digits);
+    if (digits_size == 0) {
+        *returnSize = 0;
+        return answer;
+    }
+
+    int total = 1;
+    int rows = 1;
+
+    for (int i = 0; i < digits_size; i++) {
+        if (i > 0) {
+            rows = rows * digits_length_map[digits[i] - CHR_OFFSET];
+        }
+        total = total * digits_length_map[digits[i] - CHR_OFFSET];
+    }
+
+    *returnSize = total;
+
+    int cols = total / rows;
+
+    #ifdef DEBUG
+    printf("%d %d %d %d\n", digits_size, total, rows, cols);
+    printf("----------------\n");
+    #endif
+
+    answer = (char **) malloc(sizeof(char *) * total);
+    for (int i = 0; i < total; i++) {
+        answer[i] = (char *) malloc(sizeof(char) * (digits_size + 1));
+        memset(answer[i], 0, (sizeof(char) * (digits_size + 1)));
+    }
+
+    for (int i = 0; i < total; i++) {
+        for (int j = digits_size - 1; j >=0; j--) {
+            int key_index = digits[j] - CHR_OFFSET;
+            char chr = key_board_head_map[key_index];
+            answer[i][j] = chr + (i / rows);
+            // for (int k = 0; k < j; k++) {
+            //     answer[i][j] = chr + k;
+            //     printf("%c ", chr + k);
+            // }
+        }
+        printf("\n");
+    }
+
+    // for (int i = digits_size - 1; i >= 0; i--) {
+    //     int key_index = digits[i] - CHR_OFFSET;
+    //     char chr = key_board_head_map[key_index];
+    //     for (int m = 1, j = rows * (m - 1); j < (rows * m) && m <= cols; j++, m++) {
+
+    //         for (int k = 0; k < digits_length_map[key_index]; k++) {
+    //             answer[j][i] = chr+k;
+    //             printf("%c ", chr + k);
+    //         }
+    //         printf(" | ");
+    //     }
+    //     printf("\n");
+        
+    // }
+
+
+    printf("====================\n");
+    printf("%d %d\n", total, digits_size);
+    for (int i = 0; i < total; i++) {
+        for (int j = 0; j < digits_size; j++) {
+            printf("%c", answer[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("----------------\n");
+
+    
+    return answer;
+}
+
+int main(void) {
+    int return_size = 0;
+    // printf("2\n");
+    // printf("\t%d %d %d\n", 'a', 'b', 'c');
+
+    // printf("3\n");
+    // printf("\t%d %d %d\n", 'd', 'e', 'f');
+
+    // printf("4\n");
+    // printf("\t%d %d %d\n", 'g', 'h', 'i');
+
+    // printf("5\n");
+    // printf("\t%d %d %d\n", 'j', 'k', 'l');
+
+    // printf("6\n");
+    // printf("\t%d %d %d\n", 'm', 'n', 'o');
+
+    // printf("7\n");
+    // printf("\t%d %d %d %d\n", 'p', 'q', 'r', 's');
+
+    // printf("8\n");
+    // printf("\t%d %d %d\n", 't', 'u', 'v');
+
+    // printf("9\n");
+    // printf("\t%d %d %d %d\n", 'w', 'x', 'y', 'z');
+
+
+    // letterCombinations("7239", &return_size);
+
+    letterCombinations("234", &return_size);
+    return 0;
+}
