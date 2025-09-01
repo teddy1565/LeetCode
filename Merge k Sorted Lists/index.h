6ea887a805050354789ab1c9fa05a20db86318d1
Teddy Xiong <teddy1565@gmail.com>
2025-09-01 17:39:37 +0800

    feat: 🎸 334ms beats then 5.16% | 14.20MB betas then 26.72%

---
+++ Merge k Sorted Lists/index.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+} ListNode;
+// Input: lists = [[1,4,5],[1,3,4],[2,6]]
+// Output: [1,1,2,3,4,4,5,6]
+// Explanation: The linked-lists are:
+// [
+//   1->4->5,
+//   1->3->4,
+//   2->6
+// ]
+// merging them into one sorted linked list:
+// 1->1->2->3->4->4->5->6
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize);
