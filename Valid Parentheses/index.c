524798f51eb0eb979ef168dd7c37b67a6513137c
Teddy Xiong <teddy1565@gmail.com>
2025-09-01 14:30:25 +0800

    feat: 🎸 0ms beats then 100% | 7.90MB beats then 64.90MB

--- Valid Parentheses/index.c
+++ Valid Parentheses/index.c
@@ -21,20 +21,33 @@ int get_chr_code(char c) {
 
 bool isValid(char* s) {
     int str_length = strlen(s);
-
     int current_chr_code = get_chr_code(s[0]);
-    int align_code = get_chr_code(s[0]);
-
     for (int i = 1; i < str_length; i++) {
         current_chr_code += get_chr_code(s[i]);
-
+    }
+    if (current_chr_code != 0) {
+        return false;
     }
 
-    if (current_chr_code == 0) {
-        return true;
+    int balance_list[str_length];
+    int balance_index = 0;
+    for (int i = 0; i < str_length; i++) {
+        int code = get_chr_code(s[i]);
+        if (code > 0) {
+            balance_list[balance_index++] = code;
+        } else if (code < 0) {
+            int index = --balance_index;
+            if (index < 0) {
+                return false;
+            }
+            int code_pair = balance_list[index];
+            if (code + code_pair != 0) {
+                return false;
+            }
+        }
     }
     
-    return false;
+    return true;
 }
 
 int main(void) {
