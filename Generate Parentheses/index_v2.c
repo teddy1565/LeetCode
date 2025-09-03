b03dfd31fa455968d298505774d58ff2cfaeff99
Teddy Xiong <teddy1565@gmail.com>
2025-09-03 21:22:01 +0800

    feat: 🎸 stash

--- Generate Parentheses/index_v2.c
+++ Generate Parentheses/index_v2.c
@@ -41,28 +41,39 @@ int pow_int(int x, int y) {
 }
 
 char** generateParenthesis(int n, int* returnSize) {
-    int rows = 2 << (n - 1);
-    char **bracket_list = (char **) malloc(sizeof(char *) * rows);
-
+    int max_rows = 2 << n;
     int brack_len = n * 2;
-    for (int i = 0; i < n; i++) {
+
+    char *bracket_source = (char *) malloc(sizeof(char) * (brack_len+1));
+    memset(bracket_source, 0, sizeof(char) * (brack_len + 1));
+    for (int i = 0; i < brack_len; i++) {
+        if (i >= (brack_len/2)) {
+            bracket_source[i] = ')';
+        } else {
+            bracket_source[i] = '(';
+        }
+    }
+
+    char **bracket_list = (char **) malloc(sizeof(char *) * max_rows);
+
+    for (int i = 0; i < max_rows; i++) {
         bracket_list[i] = (char *) malloc(sizeof(char) * (brack_len + 1));
         memset(bracket_list[i], 0, sizeof(char) * (brack_len + 1));
-        int k = n - i;
+        memcpy(bracket_list[i], bracket_source, sizeof(char) * brack_len);
+        int k = i / n;  // 0 0 0 0 1
+        int l = i % n;  // 0 1 2 3 0
+        int offset = n - 1;
         for (int j = 0; j < brack_len; j++) {
-            int base_g = k % n;
-            int base_x = j / k;
-            int base_y = j / n;
-            int base_z = j % k;
-
-            printf("%d\n", base_x % 2);
-            // printf("%d %d %d %d | %d %d %d\n", i, j, k, n, base_x, j % n, (base_x + 1) % 2);
-            // printf("%d %d %d | %d %d\n", base_x, base_g, base_y, j, k);
-            int index = base_x % 2;
+            int index = ((j / n)) % 2;  // 0 0 0 0 1 1 1 1
+            int g = j / n;
+            int m = j % n;
+            printf("%d %d | %d %d | %d %d\n", i, j, k, l, g, m);
             // printf("%c", bracket_dict[index]);
         }
         printf("\n");
     }
+
+    // printf("%s\n", bracket_source);
     
     return bracket_list;
 }
