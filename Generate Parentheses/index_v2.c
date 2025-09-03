8fff84bea702f80f0d05e73dd5bd8416d3df3843
Teddy Xiong <teddy1565@gmail.com>
2025-09-04 00:35:24 +0800

    feat: 🎸 trash

--- Generate Parentheses/index_v2.c
+++ Generate Parentheses/index_v2.c
@@ -1,3 +1,15 @@
+/**
+ * @file index_v2.c
+ * @author JING TING XIONG (teddy1565@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-09-04
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ * I try just use char* and calculate how much it should change. but i think not good
+ * 
+ */
 #include "index.h"
 
 const char bracket_dict[2] = { '(', ' ' };
@@ -44,6 +56,8 @@ char** generateParenthesis(int n, int* returnSize) {
     int max_rows = 2 << n;
     int brack_len = n * 2;
 
+    int base_n = pow_int(2, n*2) - pow_int(2, n);
+
     char *bracket_source = (char *) malloc(sizeof(char) * (brack_len+1));
     memset(bracket_source, 0, sizeof(char) * (brack_len + 1));
     for (int i = 0; i < brack_len; i++) {
@@ -60,14 +74,42 @@ char** generateParenthesis(int n, int* returnSize) {
         bracket_list[i] = (char *) malloc(sizeof(char) * (brack_len + 1));
         memset(bracket_list[i], 0, sizeof(char) * (brack_len + 1));
         memcpy(bracket_list[i], bracket_source, sizeof(char) * brack_len);
-        int k = i / n;  // 0 0 0 0 1
-        int l = i % n;  // 0 1 2 3 0
+        if (i == 0) {
+            continue;
+        }
+
+        int k = i / n;  // 0 0 0 0 1 1 1 1 2 2 2 2
+        int l = i % (n - 1);  // 0 1 2 3 0 1 2 3 0 1 2 3
+
+        for (int j = 0; j < brack_len; j++) {
+            // [0][3] <-> [1][0]
+            // [0][3] <-> [1][1]
+            // [0][3] <-> [1][2]
+
+
+            // [0][2] <-> [1][0]
+            // [0][2] <-> [1][1]
+            // [0][2] <-> [1][2]
+
+            // [0][3] <-> [1][0]
+            // [0][2] <-> [1][1]
+
+        }
+
+
+
+        
         int offset = n - 1;
+        int ttttt = ((i / n) * n) + (i % n);
+        printf("%d\n", ttttt);
+        int ggggg = (n * k) + (l);
         for (int j = 0; j < brack_len; j++) {
             int index = ((j / n)) % 2;  // 0 0 0 0 1 1 1 1
+            
+            
             int g = j / n;
             int m = j % n;
-            printf("%d %d | %d %d | %d %d\n", i, j, k, l, g, m);
+            // printf("%d %d | %d %d | %d %d | %d\n", i, j, k, l, g, m, ggggg);
             // printf("%c", bracket_dict[index]);
         }
         printf("\n");
