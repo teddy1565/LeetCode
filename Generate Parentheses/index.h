eb7ea546a8ebe1b8a436530351fe7ada015565fe
Teddy Xiong <teddy1565@gmail.com>
2025-09-03 18:08:36 +0800

    feat: 🎸 init

---
+++ Generate Parentheses/index.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** generateParenthesis(int n, int* returnSize);
---
+++ Generate Parentheses/index_v1.c
@@ -0,0 +1,10 @@
+#include "index.h"
+
+
+char** generateParenthesis(int n, int* returnSize) {
+    
+}
+
+int main(void) {
+    return 0;
+}
