3e4bdf51e1b4245c9829fbb91115679080fdd991
Teddy Xiong <teddy1565@gmail.com>
2025-09-10 21:07:11 +0800

    feat: 🎸 stash

--- Substring with Concatenation of All Words/index.c
+++ Substring with Concatenation of All Words/index.c
@@ -7,6 +7,11 @@ int* findSubstring(char* s, char** words, int wordsSize, int* returnSize) {
     int words_str_size = strlen(words[0]);
     int input_str_size = strlen(s);
     for (int i = 0; i < input_str_size; i+=words_str_size) {
+        printf("\tinput: ");
+        for (int l = 0; l < words_str_size; l++) {
+            printf("%c", s[i+l]);
+        }
+        printf("\n");
         for (int j = 0; j < wordsSize; j++) {
             if (s[i] == words[j][0]) {
                 for (int k = 0; k < words_str_size; k++) {
@@ -14,6 +19,7 @@ int* findSubstring(char* s, char** words, int wordsSize, int* returnSize) {
                         printf("%c ", s[i+k]);
                     }
                 }
+                printf("\n");
             }
         }
         printf("\n");
