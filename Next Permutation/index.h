a19f2469de4c318df27d8672d0ab0db2a494c829
Teddy Xiong <teddy1565@gmail.com>
2025-09-17 14:28:35 +0800

    feat: 🎸 init

---
+++ Next Permutation/index.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void nextPermutation(int* nums, int numsSize);
---
+++ Next Permutation/index_v1.c
@@ -0,0 +1,39 @@
+/**
+ * @file index_v1.c
+ * @author JING TING XIONG (teddy1565@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-09-17
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include "index.h"
+
+//arr: , , 
+
+/**
+ * @brief 
+ * [1,2,3], [1,3,2]
+ * [2,1,3], [2,3,1]
+ * [3,1,2], [3,2,1]
+ */
+
+
+/**
+ * @brief 
+ * 
+ * @param nums 
+ * @param numsSize 
+ */
+void nextPermutation(int* nums, int numsSize) {
+
+    if (numsSize == 1 || numsSize == 0) {
+        return;
+    }
+
+}
+
+int main(void) {
+    return 0;
+}
