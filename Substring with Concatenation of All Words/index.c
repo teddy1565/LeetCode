4c2d3b78d4c337307ace24939d89389eacc08c0f
Teddy Xiong <teddy1565@gmail.com>
2025-09-20 14:48:38 +0800

    feat: 🎸 79ms beats then 76.60% | 13.30MB beats then 86.79%

--- Substring with Concatenation of All Words/index.c
+++ Substring with Concatenation of All Words/index.c
@@ -5,75 +5,78 @@
  */
 int* findSubstring(char* s, char** words, int wordsSize, int* returnSize) {
     
-    int words_str_size = strlen(words[0]);
-    int input_str_size = strlen(s);
-
-    int words_answer_index = 0;
-    int *words_answer_list = (int *) malloc(sizeof(int) * 5000);
-    for (int i = 0; i < 5000; i++) {
-        words_answer_list[i] = -1;
+    if (wordsSize == 0) {
+        return NULL;
     }
 
-    int *words_index_list = (int *) malloc(sizeof(int) * wordsSize);
-    int words_index_index = 0;
+    (*returnSize) = 0;
+    const int words_str_size = strlen(words[0]);
+    const int input_str_size = strlen(s);
+
+    long long int word_total = 0;
+
+    int *answer = (int *) malloc(sizeof(int) * input_str_size);
+    int *word_head_list = (int *) malloc(sizeof(int) * 26);
+    long long int *tmp_answer = (long long int *) malloc(sizeof(long long int) * (input_str_size + 1));
+
+    memset(answer, 0, sizeof(int) * input_str_size);
+    memset(tmp_answer, 0, sizeof(long long int) * (input_str_size + 1));
+
+
+    memset(word_head_list, 0, sizeof(int) * 26);
 
     for (int i = 0; i < wordsSize; i++) {
-        words_index_list[i] = -1;
+        word_head_list[(words[i][0] - 97)] = 1;
+        long long int k_tmp = 0;
+        for (int j = 0; j < words_str_size; j++) {
+            k_tmp += (words[i][j] * words[i][j] * (j + 1));
+        }
+        word_total += (k_tmp * k_tmp);
     }
 
-    for (int i = 0; i < input_str_size; i+=words_str_size) {
-        printf("\tinput: ");
-        for (int l = 0; l < words_str_size; l++) {
-            printf("%c", s[i+l]);
+    const int word_str_total_length = words_str_size * wordsSize;
+
+    for (int i = 0; i <= (input_str_size - words_str_size); i++) {
+
+        while(i <= (input_str_size - words_str_size) && word_head_list[s[i] - 97] == 0) i++;
+
+        if ((*returnSize) > 1 && answer[(*returnSize)-1] == (i - 1) && s[i] == s[i - 1] && s[i + (word_str_total_length - 1)] == s[(i - 1)]) {
+            answer[(*returnSize)++] = i;
+            continue;
         }
-        printf("\n");
 
-
-        for (int j = 0; j < wordsSize && words_index_index < (wordsSize - 1); j++) {
-            if (s[i] == words[j][0]) {
-                int find = 1;
-                for (int k = 0; k < words_str_size; k++) {
-                    if (s[i+k] == words[j][k]) {
-                        printf("%c ", s[i+k]);
-                    } else {
-                        find = 0;
-                        break;
-                    }
-                }
-                if (find == 1) {
-                    printf("words_index_index: %d, %d\n", words_index_index, j);
-                    words_index_list[words_index_index++] = j;
-                    break;
-                }
-                printf("\n");
+        
+        
+        long long int temp_feature_total = 0;
+        for (int k = 0; k < words_str_size; k++) {
+            int s_index = i + k;
+            if (s_index > input_str_size || s[s_index] == '\0') {
+                break;
             }
-        }
-        printf("\n");
-
-        printf("words_index_index: %d\n", words_index_index);
-        if (words_index_index == (wordsSize - 1)) {
-            words_answer_list[words_answer_index++] = i;
-            
+            temp_feature_total += (s[s_index] * s[s_index] * (k + 1));
         }
 
-        words_index_index = 0;
-        for (int k = 0; k < wordsSize; k++) {
-            words_index_list[k] = -1;
+        tmp_answer[i] = (temp_feature_total * temp_feature_total);
+    }
+
+    for (int i = 0; i < input_str_size; i++) {
+        if (tmp_answer[i] == 0) {
+            continue;
+        }
+        long long int tmp_answer_total = 0;
+        for (int j = i, k = 0; j < input_str_size && k < wordsSize; j += words_str_size) {
+            tmp_answer_total += tmp_answer[j];
+            k++;
+        }
+        if (tmp_answer_total == word_total) {
+            answer[(*returnSize)++] = i;
         }
     }
 
-    printf("\n\n=================\n\n");
+    free(word_head_list);
+    free(tmp_answer);
 
-
-    // for (int i = 0; i < words_index_index; i++) {
-    //     printf("%s\n", words[words_index_list[i]]);
-    // }
-
-    for (int i = 0; i < words_answer_index; i++) {
-        printf("%d\n", words_answer_list[i]);
-    }
-
-    return NULL;
+    return answer;
 }
 
 int main(void) {
