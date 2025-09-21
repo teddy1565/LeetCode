4f6c3f289ca09979546ff728df34d92eeabf43d5
Teddy Xiong <teddy1565@gmail.com>
2025-09-21 12:37:38 +0800

    feat: 🎸 init

---
+++ Longest Valid Parentheses/index.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <stdbool.h>
+
+/**
+ * @brief Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses substring.
+ * 
+ * @param s 
+ * @return int 
+ */
+int longestValidParentheses(char* s);
---
+++ Longest Valid Parentheses/index_v1.c
@@ -0,0 +1,20 @@
+/**
+ * @file index_v1.c
+ * @author JING TING XIONG (teddy1565@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-09-21
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include "index.h"
+
+int longestValidParentheses(char* s) {
+
+}
+
+int main(void) {
+    return 0;
+}
