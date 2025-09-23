3394cfb50d7a36ab512d8c3894becd7773ad6937
Teddy Xiong <teddy1565@gmail.com>
2025-09-24 00:35:26 +0800

    feat: 🎸 0ms beats then 100% | 19.64MB beats then 46.41%

--- Move Zeros/index.c
+++ Move Zeros/index.c
@@ -1,17 +1,17 @@
 #include "index.h"
 
 
-
+//[0,1,0,3,12]
+//[4, 2, 4, 0, 0, 3, 0, 5, 1, 0]
 void moveZeroes(int* nums, int numsSize) {
-    int n = numsSize - 1;
-    for (int i = 0; i < n; i++) {
-        if (nums[i] == 0) {
-            while (nums[n] == 0) n--;
-            
-            nums[i] = nums[i] ^ nums[n];
-            nums[n] = nums[i] ^ nums[n];
-            nums[i] = nums[i] ^ nums[n];
+    int point = 0;
+    for (int i = 0, j = 0; i < numsSize; i++) {
+        if (nums[i] != 0) {
+            nums[point++] = nums[i];
         }
+    }
+    for (int i = point; i < numsSize; i++) {
+        nums[i] = 0;
     }
 }
 
--- Move Zeros/index.h
+++ Move Zeros/index.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
 
 
 void moveZeroes(int* nums, int numsSize);
