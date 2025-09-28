6c399b4ad607671f21f889177d2f76f91666de90
Teddy Xiong <teddy1565@gmail.com>
2025-09-28 19:53:27 +0800

    feat: 🎸 init

---
+++ Remove Duplicates from Sorted List/index.h
@@ -0,0 +1,8 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* deleteDuplicates(struct ListNode* head);
