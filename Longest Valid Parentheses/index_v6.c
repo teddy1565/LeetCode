5c77cbd3be0e80753488b4ed683d3d3a9c2db539
Teddy Xiong <teddy1565@gmail.com>
2025-09-28 19:34:15 +0800

    feat: 🎸 try dp

---
+++ Longest Valid Parentheses/index_v6.c
@@ -0,0 +1,48 @@
+/**
+ * @file index_v6.c
+ * @author JING TING XIONG (teddy1565@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-09-28
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include "index.h"
+
+
+// ()(()
+// )))()())
+// ())(())(
+// (()())(()()())
+int longestValidParentheses(char* s) {
+
+    int str_length = strlen(s);
+    if (str_length == 0 || str_length == 1) {
+        return 0;
+    } else if (str_length == 2) {
+        if (s[0] == '(' && s[1] == ')') return 2;
+        else return 0;
+    }
+
+    char c = '\0';
+    int a = 0;
+    int b = 1;
+    int *length_list = (int *) malloc(sizeof(int) * (str_length + 1));
+    memset(length_list, 0, sizeof(int) * (str_length + 1));
+    
+    while (s[a] != '\0' && s[b] != '\0') {
+        if (s[a] == '(' && s[b] == ')') {
+            a += 2;
+            b += 2;
+        } else if (s[a] == '(' && s[b] == '(') {
+            a++;
+            b++;
+        }
+    }
+    
+}
+
+int main(void) {
+    return 0;
+}
