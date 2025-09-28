b516280710a3f68e80a88b470dc44e1d1a536830
Teddy Xiong <teddy1565@gmail.com>
2025-09-28 20:09:29 +0800

    feat: 🎸 0ms beats then 100% | 10.96MB beats then 71.75%

--- Remove Duplicates from Sorted List/index.h
+++ Remove Duplicates from Sorted List/index.h
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+} ListNode;
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
