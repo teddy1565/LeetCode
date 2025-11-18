e73e83d063adf35e14296994f2a92cc884878084
Teddy Xiong <teddy1565@gmail.com>
2025-11-19 02:47:25 +0800

    feat: 🎸 0ms beats then 100% | 10.14MB beats then 74.52%

--- Majority Element/index_v1.c
+++ Majority Element/index_v1.c
@@ -1,20 +1,54 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+/**
+ * @brief -10^9 ~ +10^9 numsSize <= 50000
+ * 
+ * @param nums 
+ * @param numsSize 
+ * @return int 
+ */
 int majorityElement(int* nums, int numsSize) {
-    int n[50000] = { 0 };
-    for (int i = 0; i < numsSize; i++) {
-        n[nums[i]] += 1;
+    if (numsSize == 0) {
+        return 0;
+    } else if (numsSize == 1) {
+        return nums[0];
     }
-    for (int i = 0; i < 50000; i++) {
-        if (n[i] > (numsSize / 2)) {
-            return i;
+
+    int n = nums[0];
+    int k = numsSize / 2;
+    int count = 1;
+    for (int i = 1; i < numsSize; i++) {
+        if (count == 0) {
+            n = nums[i];
+        } else if (count > k) {
+            return n;
+        }
+        if (nums[i] == n) {
+            count++;
+        } else {
+            count--;
         }
     }
-    return 0;
+    return n;
 }
 
 int main(void) {
+    int test_case_01[5] = { 7, 7, 7, 7, 7 };
+    int test_case_02[1] = { 1 };
+    int test_case_03[3] = { 2, 2, 1 };
+    int test_case_04[7] = { 5, 1, 5, 2, 5, 3, 5 };
+    int test_case_05[7] = { 1, 2, 3, 4, 4, 4, 4 };
+    int test_case_06[7] = { 1, 5, 5, 5, 2, 5, 3 };
+    int test_case_07[5] = { -1, -1, 0, -1, 1 };
+    int test_case_08[5] = { 1000000000, 1000000000, 5, 1000000000, 7 };
+    printf("%d\n", majorityElement(test_case_01, 5));
+    printf("%d\n", majorityElement(test_case_02, 1));
+    printf("%d\n", majorityElement(test_case_03, 3));
+    printf("%d\n", majorityElement(test_case_04, 7));
+    printf("%d\n", majorityElement(test_case_05, 7));
+    printf("%d\n", majorityElement(test_case_06, 7));
+    printf("%d\n", majorityElement(test_case_07, 5));
+    printf("%d\n", majorityElement(test_case_08, 5));
     return 0;
 }
