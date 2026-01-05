e822a7bcf73ea6d299d896d2e32afb47b840b694
Teddy Xiong <teddy1565@gmail.com>
2026-01-05 12:44:02 +0800

    feat: 🎸 1693ms beats then 5.33% | 11.90MB beats then 37.32%

---
+++ Number of Islands/index_v1.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <limits.h>
+
+#define TABLE_SIZE 90000
+
+void grid_bfs(char** grid, int row_size, int col_size, int p_index_x, int p_index_y, int (*island_list)[2], int *island_index) {
+    int island_temp_list[TABLE_SIZE][2] = { 0 };
+    int dx[4] = { 1, -1, 0, 0 };
+    int dy[4] = { 0, 0, 1, -1 };
+    island_temp_list[0][0] = p_index_x;
+    island_temp_list[0][1] = p_index_y;
+    int island_temp_list_index = 1;
+    int update_temp_list_index = 0;
+    int temp_index = island_temp_list_index;
+    while (update_temp_list_index != temp_index) {
+        for (int i = update_temp_list_index; i < temp_index; i++) {
+            for (int j = 0; j < 4; j++) {
+                int x = island_temp_list[i][0] + dx[j];
+                int y = island_temp_list[i][1] + dy[j];
+                if (x < 0 || x >= row_size || y < 0 || y >= col_size || grid[x][y] != '1') {
+                    continue;
+                }
+                int find_repeat = 0;
+                for (int k = 0; k < island_temp_list_index; k++) {
+                    if (island_temp_list[k][0] == x && island_temp_list[k][1] == y) {
+                        find_repeat = 1;
+                        break;
+                    }
+                }
+                if (find_repeat == 1) {
+                    continue;
+                }
+                island_temp_list[island_temp_list_index][0] = x;
+                island_temp_list[island_temp_list_index][1] = y;
+                island_temp_list_index += 1;
+            }
+        }
+        update_temp_list_index = temp_index;
+        temp_index = island_temp_list_index;
+    }
+
+    for (int i = 0; i < island_temp_list_index; i++) {
+        (island_list)[(*island_index)][0] = island_temp_list[i][0];
+        (island_list)[(*island_index)][1] = island_temp_list[i][1];
+        (*island_index) += 1;
+    }
+}
+
+int numIslands(char** grid, int gridSize, int* gridColSize) {
+    
+    int island_index = 0;
+    int island_count = 0;
+    int row_size = gridSize;
+    int col_size = gridColSize[0];
+    int island_postive[TABLE_SIZE][2] = {0};
+    for (int i = 0; i < row_size; i++) {
+        for (int j = 0; j < col_size; j++) {
+            if (grid[i][j] == '1') {
+                int find_repeat = 0;
+                for (int k = 0; k < island_index; k++) {
+                    if (island_postive[k][0] == i && island_postive[k][1] == j) {
+                        find_repeat = 1;
+                        break;
+                    }
+                }
+                if (find_repeat == 1) {
+                    continue;
+                }
+                grid_bfs(grid, row_size, col_size, i, j, island_postive, &island_index);
+                island_count += 1;
+            }
+        }
+    }
+    return island_count;
+}
