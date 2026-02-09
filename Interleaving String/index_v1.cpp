28bf820ac3101042f6ad44254ae29f32eb1bee3d
TeddyXiong <teddy1565@gmail.com>
2026-02-09 16:03:12 +0800

    test: 💍 simple implements

--- Interleaving String/index_v1.cpp
+++ Interleaving String/index_v1.cpp
@@ -11,6 +11,12 @@
 
 // 需要注意DP，單純使用greedy有些可能性match不到
 class Solution {
+    private:
+        bool canInterleave(std::vector<std::vector<int>>&dp, const int s1_size, const int s2_size, int idx1, int idx2) {
+            if (idx1 >= s1_size && idx2 >= s2_size) {
+                return true;
+            }
+        }
     public:
         bool isInterleave(std::string s1, std::string s2, std::string s3) {
             int s1_size = s1.size();
@@ -20,12 +26,8 @@ class Solution {
                 return false;
             }
 
-            std::vector<std::vector<int>> dp(s3_size, std::vector<int>(2, -1));
+            std::vector<std::vector<int>> dp(s1_size + 1, std::vector<int>(s2_size + 1, -1));
 
-
-            for (int i = 1; i < s3_size; i++) {
-                
-            }
         }
 };
 
