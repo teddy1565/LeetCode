f8e79d4b92efd287d5af387a143a55e7e569b7ba
TeddyXiong <teddy1565@gmail.com>
2026-02-10 13:33:40 +0800

    test: 💍 simeple test

--- Interleaving String/index_v1.cpp
+++ Interleaving String/index_v1.cpp
@@ -12,10 +12,33 @@
 // 需要注意DP，單純使用greedy有些可能性match不到
 class Solution {
     private:
-        bool canInterleave(std::vector<std::vector<int>>&dp, const int s1_size, const int s2_size, int idx1, int idx2) {
+        bool canInterleave(std::vector<std::vector<int>>&dp, std::string & s1, std::string & s2, std::string & s3, const int s1_size, const int s2_size, int idx1, int idx2) {
             if (idx1 >= s1_size && idx2 >= s2_size) {
                 return true;
             }
+
+            if (dp[idx1][idx2] != -1) {
+                return dp[idx1][idx2] == 1;
+            }
+
+            dp[idx1][idx2] = 0;
+            int idx3 = idx1 + idx2;
+            
+            if (idx1 < s1_size && s1[idx1] == s3[idx3]) {
+                if (canInterleave(dp, s1, s2, s3, s1_size, s2_size, idx1 + 1, idx2)) {
+                    dp[idx1][idx2] = 1;
+                    return true;
+                }
+            }
+
+            if (idx2 < s2_size && s2[idx2] == s3[idx3]) {
+                if (canInterleave(dp, s1, s2, s3, s1_size, s2_size, idx1, idx2 + 1)) {
+                    dp[idx1][idx2] = 1;
+                    return true;
+                }
+            }
+
+            return dp[idx1][idx2] == 1;
         }
     public:
         bool isInterleave(std::string s1, std::string s2, std::string s3) {
@@ -28,6 +51,7 @@ class Solution {
 
             std::vector<std::vector<int>> dp(s1_size + 1, std::vector<int>(s2_size + 1, -1));
 
+            return canInterleave(dp, s1, s2, s3, s1_size, s2_size, 0, 0);
         }
 };
 
