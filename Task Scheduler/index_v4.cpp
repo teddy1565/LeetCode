a0d3f48e87d1f006829389f00806b5ce0e897b4a
TeddyXiong <teddy1565@gmail.com>
2026-03-04 13:44:42 +0800

    test: 💍 simple

---
+++ Task Scheduler/index_v4.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <unordered_map>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <stack>
+
+class Solution {
+    public:
+        int leastInterval(std::vector<char>& tasks, int n) {
+            
+        }
+};
