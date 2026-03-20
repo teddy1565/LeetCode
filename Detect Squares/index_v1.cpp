f8f73bb99fbbde81100e1978e1326700ccba65e8
Teddy Xiong <teddy1565@gmail.com>
2026-03-21 00:55:06 +0800

    feat: 🎸 init

---
+++ Detect Squares/index_v1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <unordered_map>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <cmath>
+#include <stack>
+
+
+/**
+ * Your DetectSquares object will be instantiated and called as such:
+ * DetectSquares* obj = new DetectSquares();
+ * obj->add(point);
+ * int param_2 = obj->count(point);
+ */
+class DetectSquares {
+    public:
+        DetectSquares() {
+            
+        }
+        
+        void add(std::vector<int> point) {
+            
+        }
+        
+        int count(std::vector<int> point) {
+            
+        }
+};
