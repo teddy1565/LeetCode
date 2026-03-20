53afeb80bb8937a9eaba45983efc038103ae6ada
Teddy Xiong <teddy1565@gmail.com>
2026-03-20 18:49:19 +0800

    feat: 🎸 0ms beats then 100% | 11.35MB beats then 20.15%

---
+++ Counting Bits/index_v3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <unordered_map>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <cmath>
+#include <stack>
+
+
+class Solution {
+    private:
+        static int count_bit(int n) {
+            int k = 0;
+            for (int i = 0; i < 17; i++) {
+                k += (n & 1);
+                n >>= 1;
+            }
+
+            return k;
+        }
+    public:
+        std::vector<int> countBits(int n) {
+            std::vector<int> ans;
+
+            for (int i = 0; i <= n; i++) {
+                int ans_bits = count_bit(i);
+                ans.push_back(ans_bits);
+            }
+
+            return ans;
+        }
+};
