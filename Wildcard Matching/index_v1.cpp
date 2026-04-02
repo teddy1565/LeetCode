047d6147d5f1815702ff07bfa5206f1c47f3c5fd
Teddy Xiong <teddy1565@gmail.com>
2026-04-02 20:50:56 +0800

    test: 💍 simeple code

--- Wildcard Matching/index_v1.cpp
+++ Wildcard Matching/index_v1.cpp
@@ -24,11 +24,80 @@ class Solution {
             
             std::vector<std::vector<bool>> dp(p_size, std::vector<bool>(s_size, false));
 
-            for (int i = p_size - 1; i >= 0; i--) {
-                
-                for (int j = s_size - 1; j >= 0; j--) {
-                    
+            int i = p_size - 1;
+            int j = s_size - 1;
+            while (i >= 0 && j >= 0) {
+                if (i == 0 && j == 0) {
+                    if (p[i] == s[j]) {
+                        dp[i][j] = true;
+                    } else if (p[i] == '*') {
+                        dp[i][j] = true;
+                    } else if (p[i] == '?') {
+                        dp[i][j] = true;
+                    }
+                    break;
+                }
+
+                if (i == 0 && j > 0) {
+                    while (j > 0 && i == 0) {
+                        if (p[i] == s[j]) {
+                            dp[i][j] = true;
+                            j -= 1;
+                            i -= 1;
+                        } else if (p[i] == '*') {
+                            dp[i][j] = true;
+                            j -= 1;
+                        } else if (p[i] == '?') {
+                            dp[i][j] = true;
+                            j -= 1;
+                            break;
+                        } else {
+                            return false;
+                        }
+                    }
+                } else if (i > 0 && j == 0) {
+                    while (i > 0 && j == 0) {
+                        if (p[i] == '*') {
+                            dp[i][j] = true;
+                            i -= 1;
+                        } else if (p[i] == '?') {
+                            dp[i][j] = true;
+                            i -= 1;
+                        } else if (p[i] == s[j]) {
+                            dp[i][j] = true;
+                            i -= 1;
+                            j -= 1;
+                        } else if (p[i] != s[j]) {
+                            return false;
+                        }
+                    }
+                } else if (p[i] == '*') {
+                    dp[i][j] = true;
+                    if (p[i - 1] == '?' || p[i - 1] == '*') {
+                        i = i - 1;
+                    } else {
+                        while (j > 0) {
+                            dp[i][j] = true;
+                            if (s[j] == p[i - 1]) {
+                                i -= 1;
+                                break;
+                            }
+                            j -= 1;
+                        }
+                    }
+                } else if (p[i] == '?') {
+                    dp[i][j] = true;
+                    i -= 1;
+                    j -= 1;
+                } else if (p[i] == s[j]) {
+                    dp[i][j] = true;
+                    i -= 1;
+                    j -= 1;
+                } else {
+                    break;
                 }
             }
+
+            return dp[0][0];
         }
 };
