8592705fc37d36ee3f16f5cb022e4bcecfc6dfda
teddy1565 <teddy1565@gmail.com>
2026-05-12 10:38:12 +0800

    feat: 🎸 0ms beats then 100% | 8.44MB beats then 90.89%

---
+++ Palindromic Substrings/index_v4.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <unordered_map>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <cmath>
+#include <stack>
+#include <array>
+#include <set>
+#include <numeric>
+
+// aaad
+// aaa d
+// aaa
+class Solution {
+    private:
+        static void helper(std::string & s, int a, int b, int & ans) {
+            while (a >= 0 && b < s.size() && s[a] == s[b]) {
+                a--;
+                b++;
+                ans += 1;
+            }
+        }
+    public:
+        static int countSubstrings(std::string s) {
+            if (s.empty()) {
+                return 0;
+            }
+
+            int n = s.size();
+            int ans = 0;
+
+            for (int i = 0; i < n; i++) {
+                helper(s, i, i, ans);
+                helper(s, i, i + 1, ans);
+            }
+            return ans;
+        }
+};
